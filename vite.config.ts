cda02d062e1bdfc233ee03bf2f05c2ba554047ac
nicholasxjy <nicholasxjy@gmail.com>
2024-03-12 21:01:16 +0800

    chore: release 0.0.3

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
